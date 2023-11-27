26b284ea4f2b0815f900933b99829956763a7c14
Vinicio Shurman <48026024+vinicioShurman@users.noreply.github.com>
2023-11-27 19:59:10 -0300

    Its working, but need more tests

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
+import os
 import docx2txt
 
-input_loc = input("Your docx location: ")
-output_loc = input("Output location: ")
+# This code extracts all images of all files in the directory the script is running
 
-text = docx2txt.process(input_loc, output_loc)
+def find_word_files(directory):
+    # Check if the specified directory exists
+    if not os.path.exists(directory):
+        print(f"Falha ao encontrar o diretorio: '{directory}'")
+        return []
 
-# Don't forget to 
+    word_files = []
+
+    # List all files in the directory
+    files = os.listdir(directory)
+
+    # Filter files with ".docx" and ".doc" extensions
+    word_files = [file for file in files if file.endswith((".docx", ".doc"))]
+
+    return word_files
+
+# Get the directory of the current script '__file__' is special variable in Python holds the path of the script that is currently being executed.
+directory_path = os.path.dirname(os.path.abspath(__file__))
+
+word_files_found = find_word_files(directory_path)
+
+
+if word_files_found:
+    print("Arquivos encontrados em {}: {}".format(directory_path, word_files_found))
+else:
+    print("Nenhum arquivo .doc ou .docx encontrado no diretorio")
+
+for word in word_files_found:
+    folder_path = os.path.join(directory_path, "Imagens_" + word)
+
+    os.makedirs(folder_path, exist_ok=True)
+
+    text = docx2txt.process(word, folder_path)
